6712ce8428d8a5600399e322e2d06639379ae8d2
AbdulFattaah Popoola <abdulapopoola@outlook.com>
2026-01-01 19:46:11 -0800

    build: configure eslint for es modules

--- adehun.js
+++ adehun.js
@@ -1,196 +1,211 @@
-"use strict";
+'use strict';
 
 const validStates = {
-    PENDING: 0,
-    FULFILLED: 1,
-    REJECTED: 2
+  PENDING: 0,
+  FULFILLED: 1,
+  REJECTED: 2,
 };
 
 const isValidState = (state) => {
-    return ((state === validStates.PENDING) ||
-            (state === validStates.REJECTED) ||
-            (state === validStates.FULFILLED));
+  return (
+    state === validStates.PENDING ||
+    state === validStates.REJECTED ||
+    state === validStates.FULFILLED
+  );
 };
 
 const Utils = {
-    runAsync: (fn) => {
-        setTimeout(fn, 0);
-    },
-    isFunction: (val) => {
-        return val && typeof val === "function";
-    },
-    isObject: (val) => {
-        return val && typeof val === "object";
-    },
-    isPromise: (val) => {
-        return val && val instanceof Adehun;
-    }
+  runAsync: (fn) => {
+    setTimeout(fn, 0);
+  },
+  isFunction: (val) => {
+    return val && typeof val === 'function';
+  },
+  isObject: (val) => {
+    return val && typeof val === 'object';
+  },
+  isPromise: (val) => {
+    return val && val instanceof Adehun;
+  },
 };
 
 class Adehun {
-    constructor(fn) {
-        this.value = null;
-        this.state = validStates.PENDING;
-        this.queue = [];
-        this.handlers = {
-            fulfill: null,
-            reject: null
-        };
+  constructor(fn) {
+    this.value = null;
+    this.state = validStates.PENDING;
+    this.queue = [];
+    this.handlers = {
+      fulfill: null,
+      reject: null,
+    };
 
-        if (fn) {
-            fn(value => {
-                Resolve(this, value);
-            }, reason => {
-                this.reject(reason);
-            });
+    if (fn) {
+      fn(
+        (value) => {
+          Resolve(this, value);
+        },
+        (reason) => {
+          this.reject(reason);
         }
+      );
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    const queuedPromise = new Adehun();
+    if (Utils.isFunction(onFulfilled)) {
+      queuedPromise.handlers.fulfill = onFulfilled;
     }
 
-    then(onFulfilled, onRejected) {
-        const queuedPromise = new Adehun();
-        if (Utils.isFunction(onFulfilled)) {
-            queuedPromise.handlers.fulfill = onFulfilled;
+    if (Utils.isFunction(onRejected)) {
+      queuedPromise.handlers.reject = onRejected;
+    }
+
+    this.queue.push(queuedPromise);
+    this.process();
+
+    return queuedPromise;
+  }
+
+  transition(state, value) {
+    if (
+      this.state === state ||
+      this.state !== validStates.PENDING ||
+      !isValidState(state) ||
+      arguments.length !== 2
+    ) {
+      return;
+    }
+
+    this.value = value;
+    this.state = state;
+    this.process();
+  }
+
+  process() {
+    const fulfillFallBack = (value) => {
+      return value;
+    };
+    const rejectFallBack = (reason) => {
+      throw reason;
+    };
+
+    if (this.state === validStates.PENDING) {
+      return;
+    }
+
+    Utils.runAsync(() => {
+      while (this.queue.length) {
+        let queuedPromise = this.queue.shift(),
+          handler = null,
+          value;
+
+        if (this.state === validStates.FULFILLED) {
+          handler = queuedPromise.handlers.fulfill || fulfillFallBack;
+        } else if (this.state === validStates.REJECTED) {
+          handler = queuedPromise.handlers.reject || rejectFallBack;
         }
 
-        if (Utils.isFunction(onRejected)) {
-            queuedPromise.handlers.reject = onRejected;
+        try {
+          value = handler(this.value);
+        } catch (e) {
+          queuedPromise.transition(validStates.REJECTED, e);
+          continue;
         }
 
-        this.queue.push(queuedPromise);
-        this.process();
+        Resolve(queuedPromise, value);
+      }
+    });
+  }
 
-        return queuedPromise;
-    }
+  fulfill(value) {
+    this.transition(validStates.FULFILLED, value);
+  }
 
-    transition(state, value) {
-        if (this.state === state ||
-            this.state !== validStates.PENDING ||
-            !isValidState(state) ||
-            arguments.length !== 2) {
-            return;
-        }
+  reject(reason) {
+    this.transition(validStates.REJECTED, reason);
+  }
 
-        this.value = value;
-        this.state = state;
-        this.process();
-    }
+  static resolved(value) {
+    return new Adehun((resolve) => {
+      resolve(value);
+    });
+  }
 
-    process() {
-        const fulfillFallBack = (value) => {
-            return value;
-        };
-        const rejectFallBack = (reason) => {
-            throw reason;
-        };
+  static rejected(reason) {
+    return new Adehun((resolve, reject) => {
+      reject(reason);
+    });
+  }
 
-        if (this.state === validStates.PENDING) {
-            return;
-        }
-
-        Utils.runAsync(() => {
-            while (this.queue.length) {
-                let queuedPromise = this.queue.shift(),
-                    handler = null,
-                    value;
-
-                if (this.state === validStates.FULFILLED) {
-                    handler = queuedPromise.handlers.fulfill || fulfillFallBack;
-                } else if (this.state === validStates.REJECTED) {
-                    handler = queuedPromise.handlers.reject || rejectFallBack;
-                }
-
-                try {
-                    value = handler(this.value);
-                } catch (e) {
-                    queuedPromise.transition(validStates.REJECTED, e);
-                    continue;
-                }
-
-                Resolve(queuedPromise, value);
-            }
-        });
-    }
-
-    fulfill(value) {
-        this.transition(validStates.FULFILLED, value);
-    }
-
-    reject(reason) {
-        this.transition(validStates.REJECTED, reason);
-    }
-
-    static resolved(value) {
-        return new Adehun(resolve => {
-            resolve(value);
-        });
-    }
-
-    static rejected(reason) {
-        return new Adehun((resolve, reject) => {
-            reject(reason);
-        });
-    }
-
-    static deferred() {
-        let resolve, reject;
-        const promise = new Adehun((rslv, rjct) => {
-            resolve = rslv;
-            reject = rjct;
-        });
-        return {
-            promise: promise,
-            resolve: resolve,
-            reject: reject
-        };
-    }
+  static deferred() {
+    let resolve, reject;
+    const promise = new Adehun((rslv, rjct) => {
+      resolve = rslv;
+      reject = rjct;
+    });
+    return {
+      promise: promise,
+      resolve: resolve,
+      reject: reject,
+    };
+  }
 }
 
 function Resolve(promise, x) {
-    if (promise === x) {
-        promise.transition(validStates.REJECTED, new TypeError("The promise and its value refer to the same object"));
-    } else if (Utils.isPromise(x)) {
-        if (x.state === validStates.PENDING) {
-            x.then(val => {
-                Resolve(promise, val);
-            }, reason => {
-                promise.transition(validStates.REJECTED, reason);
-            });
-        } else {
-            promise.transition(x.state, x.value);
+  if (promise === x) {
+    promise.transition(
+      validStates.REJECTED,
+      new TypeError('The promise and its value refer to the same object')
+    );
+  } else if (Utils.isPromise(x)) {
+    if (x.state === validStates.PENDING) {
+      x.then(
+        (val) => {
+          Resolve(promise, val);
+        },
+        (reason) => {
+          promise.transition(validStates.REJECTED, reason);
         }
-    } else if (Utils.isObject(x) || Utils.isFunction(x)) {
-        let called = false,
-            thenHandler;
-        try {
-            thenHandler = x.then;
-
-            if (Utils.isFunction(thenHandler)) {
-                thenHandler.call(x,
-                    y => {
-                        if (!called) {
-                            Resolve(promise, y);
-                            called = true;
-                        }
-                    },
-                    r => {
-                        if (!called) {
-                            promise.reject(r);
-                            called = true;
-                        }
-                    });
-            } else {
-                promise.fulfill(x);
-                called = true;
-            }
-        } catch (e) {
-            if (!called) {
-                promise.reject(e);
-                called = true;
-            }
-        }
+      );
     } else {
-        promise.fulfill(x);
+      promise.transition(x.state, x.value);
     }
+  } else if (Utils.isObject(x) || Utils.isFunction(x)) {
+    let called = false,
+      thenHandler;
+    try {
+      thenHandler = x.then;
+
+      if (Utils.isFunction(thenHandler)) {
+        thenHandler.call(
+          x,
+          (y) => {
+            if (!called) {
+              Resolve(promise, y);
+              called = true;
+            }
+          },
+          (r) => {
+            if (!called) {
+              promise.reject(r);
+              called = true;
+            }
+          }
+        );
+      } else {
+        promise.fulfill(x);
+        called = true;
+      }
+    } catch (e) {
+      if (!called) {
+        promise.reject(e);
+        called = true;
+      }
+    }
+  } else {
+    promise.fulfill(x);
+  }
 }
 
-module.exports = Adehun;
+export default Adehun;
--- testrunner.js
+++ testrunner.js
@@ -1,7 +1,7 @@
-const promisesAplusTests = require("promises-aplus-tests");
-const Adehun = require("./adehun.js");
+import promisesAplusTests from 'promises-aplus-tests';
+import Adehun from './adehun.js';
 
-promisesAplusTests(Adehun, function (err) {
-    // All done; output is in the console. Or check `err` for number of failures.
-    console.log(err);
+promisesAplusTests(Adehun, err => {
+  // All done; output is in the console. Or check `err` for number of failures.
+  console.log(err);
 });
